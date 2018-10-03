6d37df9e25c6e070ef1ea99b9bdc53f07611c822
Matthew Rodusek <matthew.rodusek@gmail.com>
2018-10-02 21:46:04 -0400

    Change conan package deployment format

--- .ci/deploy_conan.sh
+++ .ci/deploy_conan.sh
@@ -8,7 +8,10 @@ set -e
 
 function usage()
 {
-  echo "usage: ${0} <channel> [version suffix]"
+  echo "usage: ${0} [stable|edge|testing]"
+  echo " - stable:  for master branch. Every merge must be unique version."
+  echo " - edge:    for release branches. Suffixed by number of merges to release not on develop."
+  echo " - testing: for develop branch. Suffixed by number of merges to develop not on master."
 }
 
 ##############################################################################
@@ -21,7 +24,7 @@ if [[ $# -lt 1 ]]; then
   exit 1
 fi
 
-if [[ $# -gt 2 ]]; then
+if [[ $# -gt 1 ]]; then
   echo >&2 "${0}: error: too many arguments specified"
   usage
   exit 1
@@ -35,11 +38,27 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 repo_root=$(dirname $script_dir)
 
 channel="$1"
-if [[ $# -eq 2 ]]; then
-  export BUILD_SUFFIX="$2"
+
+if [[ "$1" == "stable" ]]; then
+  :
+elif [[ "$1" == "edge" ]]; then
+  # First release branch will always have 0 merges (from the initial push)
+  count=$(git rev-list --count --merges HEAD ^develop)
+  count=$((count+1))
+  export BUILD_SUFFIX="beta.${count}"
+elif [[ "$1" == "testing" ]]; then
+  hash=$(git rev-parse HEAD)
+  count=$(git rev-list --count --merges HEAD ^master)
+  count=$((count+1))
+  export BUILD_SUFFIX="alpha.${count}+${hash}"
+else
+  echo >&2 "${0}: error: 1st positional argument must be 1 of 'stable', 'edge', or 'testing'"
+  usage
+  exit 1
 fi
 
 (
+
   cd "${repo_root}"
   # Deploy to conan
   conan user -p "${BINTRAY_API_KEY}" -r cppbits cppbits
