a6240677a92f5eb05dcd071453bbc016744ec80b
Matthew Rodusek <matthew.rodusek@gmail.com>
2018-10-12 21:42:12 -0400

    Update conan deployment script

--- .travis/deploy_conan.sh
+++ .travis/deploy_conan.sh
@@ -8,10 +8,10 @@ set -e
 
 function usage()
 {
-  echo "usage: ${0} [stable|edge|testing]"
+  echo "usage: ${0} [stable|prerelease|edge]"
   echo " - stable:  for master branch. Every merge must be unique version."
+  echo " - prerelease: for develop branch. Suffixed by number of merges to develop not on master."
   echo " - edge:    for release branches. Suffixed by number of merges to release not on develop."
-  echo " - testing: for develop branch. Suffixed by number of merges to develop not on master."
 }
 
 ##############################################################################
@@ -37,28 +37,28 @@ fi
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 repo_root=$(dirname $script_dir)
 
-channel="$1"
-
 if [[ "$1" == "stable" ]]; then
-  :
-elif [[ "$1" == "edge" ]]; then
+  channel="stable"
+elif [[ "$1" == "prerelease" ]]; then
+  channel="stable"
   # First release branch will always have 0 merges (from the initial push)
   count=$(git rev-list --count --merges HEAD ^develop)
   count=$((count+1))
-  export CONAN_VERSION_SUFFIX="beta.${count}"
-elif [[ "$1" == "testing" ]]; then
+  export CONAN_VERSION_SUFFIX="prerelease.${count}"
+elif [[ "$1" == "edge" ]]; then
+  channel="testing"
   hash=$(git rev-parse HEAD)
+  # First release branch will always have 0 merges (from the initial push)
   count=$(git rev-list --count --merges HEAD ^master)
   count=$((count+1))
-  export CONAN_VERSION_SUFFIX="alpha.${count}+${hash}"
+  export CONAN_VERSION_SUFFIX="snapshot.${count}+sha.${hash}"
 else
-  echo >&2 "${0}: error: 1st positional argument must be 1 of 'stable', 'edge', or 'testing'"
+  echo >&2 "${0}: error: 1st positional argument must be 1 of 'stable', 'edge', or 'prerelease'"
   usage
   exit 1
 fi
 
 (
-
   cd "${repo_root}"
   # Deploy to conan
   conan user -p "${BINTRAY_API_KEY}" -r cppbits cppbits
